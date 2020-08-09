adbab1fc70dd00fc935280b6a366141ecd330c6a
Kelvin Lima de Souza <kelvin.souza22@etec.sp.gov.br>
2020-08-09 02:42:15 -0300

    Mudança BD

--- noticia/bdnoticia.sql
+++ noticia/bdnoticia.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 12-Set-2019 às 18:56
--- Versão do servidor: 5.7.9
--- PHP Version: 5.6.16
+-- Tempo de geração: 09-Ago-2020 às 07:40
+-- Versão do servidor: 10.4.13-MariaDB
+-- versão do PHP: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,57 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bdnoticia`
+-- Banco de dados: `bdnoticia`
 --
-CREATE DATABASE IF NOT EXISTS `bdnoticia` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `bdnoticia`;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `categoria`
---
-
-DROP TABLE IF EXISTS `categoria`;
-CREATE TABLE IF NOT EXISTS `categoria` (
-  `codcategoria` int(11) NOT NULL AUTO_INCREMENT,
-  `nomecategoria` varchar(50) NOT NULL,
-  PRIMARY KEY (`codcategoria`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `categoria`
---
-
-INSERT INTO `categoria` (`codcategoria`, `nomecategoria`) VALUES
-(13, 'Esportes'),
-(14, 'Games');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `noticia`
---
-
-DROP TABLE IF EXISTS `noticia`;
-CREATE TABLE IF NOT EXISTS `noticia` (
-  `codnoticia` int(11) NOT NULL AUTO_INCREMENT,
-  `titulo` varchar(50) NOT NULL,
-  `autor` varchar(50) NOT NULL,
-  `data` date NOT NULL,
-  `conteudo` text NOT NULL,
-  `imagem` varchar(50) NOT NULL,
-  `codcategoria` int(11) NOT NULL,
-  PRIMARY KEY (`codnoticia`),
-  KEY `codcategoria` (`codcategoria`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `noticia`
---
-
-INSERT INTO `noticia` (`codnoticia`, `titulo`, `autor`, `data`, `conteudo`, `imagem`, `codcategoria`) VALUES
-(4, 'Responsabilidade TÃ©cnica e Social', 'Anderson Spera', '2019-09-06', '<p>zxzc\\</p>\r\n', 'sonic.jpg', 13);
 
 -- --------------------------------------------------------
 
@@ -75,38 +27,41 @@ INSERT INTO `noticia` (`codnoticia`, `titulo`, `autor`, `data`, `conteudo`, `ima
 -- Estrutura da tabela `usuario`
 --
 
-DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `codusuario` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `usuario` (
+  `codusuario` int(11) NOT NULL,
   `nome` varchar(70) NOT NULL,
   `email` varchar(100) NOT NULL,
   `senha` varchar(40) NOT NULL,
-  `nivel_acesso` int(11) NOT NULL,
-  PRIMARY KEY (`codusuario`)
-) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
+  `nivel_acesso` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
 INSERT INTO `usuario` (`codusuario`, `nome`, `email`, `senha`, `nivel_acesso`) VALUES
-(18, 'teste de cadastro', 'teste@teste.com', 'asklfasj', 2),
-(24, 'Anderson Spera', 'andersonspera@gmail.com', '2e76fd7955d350d5754d132e6e96a6ffc3f99b2b', 1),
-(25, 'Anderson Spera', 'andersonspera@gmail.com', '793c41bce8b83711d498a0b6f62a22f4802bee04', 1),
-(26, 'Anderson Spera', 'andersonspera@gmail.com', '8bdf64501f3e21522d3996e266c8eff7055885bd', 1),
-(27, 'Anderson Spera', 'andersonspera@gmail.com', 'df211ccdd94a63e0bcb9e6ae427a249484a49d60', 1),
-(28, 'Anderson Spera', 'andersonspera@gmail.com', 'bfff0147b7805dd47fadf3fb41642184888c0779', 1);
+(30, 'kelvin', 'k@k.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 1);
 
 --
--- Constraints for dumped tables
+-- Índices para tabelas despejadas
 --
 
 --
--- Limitadores para a tabela `noticia`
+-- Índices para tabela `usuario`
 --
-ALTER TABLE `noticia`
-  ADD CONSTRAINT `noticia_ibfk_1` FOREIGN KEY (`codcategoria`) REFERENCES `categoria` (`codcategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `noticia_ibfk_2` FOREIGN KEY (`codcategoria`) REFERENCES `categoria` (`codcategoria`);
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`codusuario`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `codusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
